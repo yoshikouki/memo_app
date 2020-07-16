64ddca3890c4483513f4c61567337b3c6efe415f
yoshikouki <yoshikouki@gmail.com>
2020-07-16 11:33:45 +0900

    update/ Create 処理のリダイレクトを実装

--- lib/memo.rb
+++ lib/memo.rb
@@ -30,7 +30,7 @@ end
 # create
 post '/' do
   access = Memo::Accessor.new(params['title'])
-  redirect '/' and return if access.validate_create
+  require_unique_title
 
   memo = access.create_content(text: params[:text])
   redirect "/#{memo.title}"
@@ -47,9 +47,7 @@ end
 put '/:path' do
   memo = fetch_memo(request.path)
   new_memo = Memo::Content.new(params['title'])
-  if @validation.validate_update(memo, new_memo)
-    memo.update(new_memo.title, params['text'])
-  end
+  memo.update(new_memo.title, params['text']) if @validation.validate_update(memo, new_memo)
   redirect "/#{memo.title}"
 end
 
@@ -68,6 +66,10 @@ helpers do
 
   def require_memo_existed(access)
     redirect '/' unless access.exist_memo?
+  end
+
+  def require_unique_title
+    redirect '/' if access.exist_memo?
   end
 end
 
